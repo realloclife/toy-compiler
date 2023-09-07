ce3b7fe95548920da0c4dfe002a3ea7d1fd26821
realloclife <carelessxne1@gmail.com>
2023-09-07 12:46:19 +0300

    Implement parsing for let & return

--- parse.py
+++ parse.py
@@ -1,6 +1,10 @@
-from typing import Optional, List
+from typing import Optional, List, Union, cast
+from enum import Enum, auto
 
 import lex
+
+class UnexpectedTokenError(Exception):
+    pass
 
 class Node:
     pass
@@ -11,29 +15,54 @@ class Statement(Node):
 class Expression(Node):
     pass
 
+class Leaf(Expression):
+    class Kind(Enum):
+        IDENTIFIER = auto()
+        STRING = auto()
+        BOOL = auto()
+        NUMBER = auto()
+
+    def __init__(self, token: lex.Token):
+        token.literal = cast(str, token.literal) # safe
+        match token.type:
+            case lex.TokenType.IDENTIFIER:
+                self.kind = self.Kind.IDENTIFIER
+                self.value = token.literal
+            case lex.TokenType.STRING:
+                self.kind = self.Kind.STRING
+                self.value = token.literal
+            case lex.TokenType.BOOL:
+                self.kind = self.Kind.BOOL
+                self.value = token.literal == 'true'
+            case lex.TokenType.NUMBER:
+                self.kind = self.Kind.NUMBER
+                self.value = float(token.literal)
+            case _:
+                raise UnexpectedTokenError(self.Kind, token)
+    
+    def __repr__(self) -> str:
+        return f'Leaf(EXPR)[Kind: {self.kind}, Value: {self.value}]'
+
 class Let(Statement):
     def __init__(self, identifier: str, value: Expression):
         self.identifier = identifier
         self.value = value
 
     def __repr__(self) -> str:
-        return f'Statement[Kind: Let, Identifier: {self.identifier}, Value: {self.value}]'
+        return f'Let(STMT)[Identifier: {self.identifier}, Value: {self.value}]'
 
 class Return(Statement):
     def __init__(self, value: Expression):
         self.value = value
 
     def __repr__(self) -> str:
-        return f'Statement[Kind: Return, Value: {self.value}]'
-
-class UnexpectedTokenError(Exception):
-    pass
+        return f'Return(STMT)[Value: {self.value}]'
 
 class Parser:
     def __init__(self, source: List[lex.Token]):
         self.input = source
         self.index = -1
-        self.curr: lex.Token = None
+        self.curr = cast(lex.Token, None)
 
     def advance(self) -> bool:
         if self.index + 1 < len(self.input):
@@ -42,6 +71,11 @@ class Parser:
             return True
         else:
             return False
+    
+    def consume(self) -> lex.Token:
+        if not self.advance():
+            raise UnexpectedTokenError(lex.Token, None)
+        return self.curr
 
     def peek(self, count: int = 1) -> Optional[lex.Token]:
         if self.index + count < len(self.input) and self.index + count >= 0:
@@ -57,16 +91,30 @@ class Parser:
             return None
     
     def expect(self, type: lex.TokenType) -> lex.Token:
-        if (peek := self.peek()) is None or peek.type != type:
-            raise UnexpectedTokenError(type, peek)
-        else:
-            return peek
+        if not self.advance() or self.curr.type != type:
+            raise UnexpectedTokenError(type, self.curr)
+        return self.curr
     
     def parse_expression(self) -> Expression:
-        pass
+        match self.curr.type:
+            case lex.TokenType.IDENTIFIER | lex.TokenType.STRING | lex.TokenType.BOOL | lex.TokenType.NUMBER:
+                return Leaf(self.curr)
+        return cast(Expression, None) # TODO
 
     def parse_statement(self) -> Optional[Statement]:
-        pass
+        if not self.advance():
+            return None
+        match self.curr.type:
+            case lex.TokenType.KEYWORD_RETURN:
+                self.consume()
+                return Return(self.parse_expression())
+            case lex.TokenType.KEYWORD_LET:
+                identifier = cast(str, self.consume().literal)
+                self.expect(lex.TokenType.ASSIGN)
+                self.consume()
+                return Let(identifier, self.parse_expression())
+            case illegal:
+                raise UnexpectedTokenError(Statement, illegal)
     
     def get_tree(self) -> List[Statement]:
         statements = []
--- repl.py
+++ repl.py
@@ -1,26 +1,37 @@
+from typing import List, cast
 import sys
 
 import lex
+import parse
 
 def main():
-    if len(sys.argv) == 2:
-        with open(sys.argv[1]) as file:
-            source = file.read()
-            tokens = lex.Lexer(source).get_tokens()
-            for token in tokens:
-                print(token)
-            raise SystemExit
+    # if len(sys.argv) == 2:
+    #     with open(sys.argv[1]) as file:
+    #         source = file.read()
+    #         tokens = cast(List[lex.Token], [])
+    #         try:
+    #             tokens = lex.Lexer(source).get_tokens()
+    #         except lex.IllegalLexemeError as e:
+    #             print(f'Invalid lexeme \'{e.args[0]}\'')
+    #         for token in tokens:
+    #             print(token)
+    #         raise SystemExit
     while True:
         source = input('> ')
         if source == 'exit':
             raise SystemExit
-        tokens = []
+        tokens = cast(List[lex.Token], None)
         try:
             tokens = lex.Lexer(source).get_tokens()
         except lex.IllegalLexemeError as e:
             print(f'Invalid lexeme \'{e.args[0]}\'')
-        for token in tokens:
-            print(token)
+        syntax_tree = cast(List[parse.Statement], None)
+        try:
+            syntax_tree = parse.Parser(tokens).get_tree()
+        except parse.UnexpectedTokenError as e:
+            print(f'Expected {e.args[0]}, got {e.args[1]} instead')
+        for statement in syntax_tree:
+            print(statement)
 
 if __name__ == '__main__':
     main()
